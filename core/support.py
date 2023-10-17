d8030666249bf96c56cf52717ad0fe8d0f427107
kalebbroo <kalebbroo@gmail.com>
2023-10-16 23:30:59 -0400

    select menu

--- core/support.py
+++ core/support.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ui import View, Button, Select
+from discord.ui import View, Button, Select, Modal, TextInput
 from discord import ButtonStyle, Interaction, ui
 from discord import Colour
 from datetime import datetime, timedelta
@@ -55,10 +55,7 @@ class Support(commands.Cog):
             self.database = database_cog
             self.bot = bot
 
-            for button_name, role_info in role_mapping.items():
-                #print(f"Adding button: {button_name}, Role ID: {role_info['role_id']}, Emoji: {role_info['emoji']}")
-                # Pass the bot instance when creating the RoleButton
-                self.add_item(Support.TicketButton(self.bot, label=button_name, role_id=role_info['role_id'], emoji=role_info['emoji']))
+            self.add_item(Support.TicketButton(self.bot, label=button_name))
 
     class TicketButton(Button):
         cooldown_users = {}
@@ -69,7 +66,7 @@ class Support(commands.Cog):
             self.role_id = role_id
 
         @discord.ui.button(style=ButtonStyle.success, label="Create Support Ticket", custom_id="support_ticket", row=1)
-        async def regenerate(self, interaction, button):
+        async def support_ticket(self, interaction, button):
             await interaction.response.defer(ephermal=True)
             select_menu = discord.SelectMenu(custom_id='ticket_type', placeholder='Choose a ticket type...', options=[
             discord.SelectOption(label='General Inquiry', value='general'),
@@ -82,16 +79,17 @@ class Support(commands.Cog):
             await interaction.response.send_message("Please choose the type of ticket you'd like to create:", components=[action_row], ephemeral=True)
 
         @discord.ui.button(style=ButtonStyle.success, label="Report A User", custom_id="report", row=1)
-        async def regenerate(self, interaction, button):
-            await interaction.response.defer(ephermal=True)
-            await interaction.response.send_message("Report User button clicked!", ephemeral=True)
+        async def report(self, interaction, button):
+            await interaction.response.defer(ephemeral=True)
+            modal = Support.ReportModal(self.bot, interaction, interaction.channel)
+            await modal.start(interaction)
 
         @discord.ui.button(style=ButtonStyle.success, label="How to Use", custom_id="how_to", row=1)
-        async def regenerate(self, interaction, button):
+        async def how_to(self, interaction, button):
             await interaction.response.defer(ephermal=True)
 
         @discord.ui.button(style=ButtonStyle.success, label="Commission Requests", custom_id="commission", row=2)
-        async def regenerate(self, interaction, button):
+        async def commissions(self, interaction, button):
             await interaction.response.defer(ephemeral=True)
             await interaction.response.send_message("Report User button clicked!", ephemeral=True)
 
@@ -103,9 +101,9 @@ class Support(commands.Cog):
         # TODO: correctly pass channel info
         
         # Create a private thread for the ticket
-        guild = discord.utils.get(self.bot.guilds)
-        staff_channel = discord.utils.get(guild.text_channels, name='staff')
-        support_channel = discord.utils.get(guild.text_channels, name='support')
+        guild = interaction.guild
+        support_channel = self.find_channel_by_name(guild, 'support')  # Assuming you have this utility function
+        staff_channel = self.find_channel_by_name(guild, 'staff')  # Assuming you have this utility function
         
         thread = await support_channel.create_text_channel(f"{selected_value}-ticket")
         
@@ -127,8 +125,14 @@ class Support(commands.Cog):
     class ChooseTicket(Select):
         def __init__(self, bot):
             self.bot = bot
-
-            # TODO: add the choices for what type of models they are opening a ticket for
+            options = [
+                discord.SelectOption(label='Pack From MCModels', value='option1'),
+                discord.SelectOption(label='Patreon Model', value='option2'),
+                discord.SelectOption(label='Free Model', value='option2'),
+                discord.SelectOption(label='LRD Plugins', value='option2'),
+                discord.SelectOption(label='Other', value='option2'),
+            ]
+            super().__init__(custom_id='choose_ticket', placeholder='Choose a ticket type...', options=options)
 
         async def callback(self, interaction):
             await interaction.response.defer()
@@ -144,8 +148,12 @@ class Support(commands.Cog):
     class SelectProduct(Select):
         def __init__(self, bot):
             self.bot = bot
-
-            # TODO: add the choices for specific products
+            options = [
+                discord.SelectOption(label='Product1', value='product1'),
+                discord.SelectOption(label='Product2', value='product2'),
+                # Add more options here
+            ]
+            super().__init__(custom_id='select_product', placeholder='Choose a product...', options=options)
 
         async def callback(self, interaction):
             await interaction.response.defer()
@@ -153,6 +161,50 @@ class Support(commands.Cog):
             specific_product = self.values[0]
             # TODO: add the logic to create ticket in a private thread
             await self.ticket_type_selected(self, interaction)
+
+
+    class ReportModal(Modal):
+        def __init__(self, bot, interaction, channel):
+            super().__init__(title="Report a User")
+            self.bot = bot
+            self.channel = channel
+            
+            self.user_id_input = TextInput(label='Enter the User ID to report',
+                                        style=discord.TextStyle.short,
+                                        placeholder='Enter User ID',
+                                        min_length=1,
+                                        max_length=20,
+                                        required=True)
+            self.add_item(self.user_id_input)
+            self.reason_input = TextInput(label='Enter the reason for reporting',
+                                        style=discord.TextStyle.long,
+                                        placeholder='Enter your reason here',
+                                        min_length=1,
+                                        max_length=4000,
+                                        required=True)
+            self.add_item(self.reason_input)
+
+        async def on_submit(self, interaction):
+            reported_user_id = self.user_id_input.value
+            reason = self.reason_input.value
+            guild = interaction.guild
+            staff_channel = discord.utils.get(guild.text_channels, name='staff')
+            
+            # Validate that the user ID exists in the server
+            reported_user = guild.get_member(int(reported_user_id))
+            if not reported_user:
+                await interaction.followup.send("The user ID provided does not belong to this server.", ephemeral=True)
+                return
+            # Notify the user that the report has been submitted
+            await interaction.followup.send("Your report has been submitted.", ephemeral=True)
+            
+            # Send an embed to the staff channel
+            embed = discord.Embed(title="New User Report", color=discord.Colour.red())
+            embed.add_field(name="Reported User ID", value=reported_user_id, inline=False)
+            embed.add_field(name="Reason for Report", value=reason, inline=False)
+            
+            await staff_channel.send(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Support(bot))
