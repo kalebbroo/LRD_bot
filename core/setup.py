448ac2e0f159982457e8cc442661d572f589a8f4
kalebbroo <kalebbroo@gmail.com>
2023-10-31 10:30:21 -0400

    updated db method calls to follow new structure

--- core/setup.py
+++ core/setup.py
@@ -14,6 +14,7 @@ class SetupSelect(Select):
     async def callback(self, interaction):
         selection = self.values[0]
         embed_cog = self.bot.get_cog("CreateEmbed")
+        db_cog = self.bot.get_cog("Database")
 
         match selection:
             case "Add FAQ":
@@ -21,8 +22,7 @@ class SetupSelect(Select):
                 await interaction.response.send_modal(view)
             
             case "Remove FAQ":
-                database_cog = self.bot.get_cog("Database")
-                all_faqs = await database_cog.get_all_faqs(interaction.guild.id)
+                all_faqs = await db_cog.handle_faq(interaction.guild.id, "get_all")
                 faq_msg = "\n".join([f"#{faq[0]} - {faq[1]}" for faq in all_faqs])
                 embed = await embed_cog.create_embed(title="FAQs", description=faq_msg, color=Colour.blue())
                 await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -41,23 +41,23 @@ class SetupSelect(Select):
                 await interaction.response.send_modal(modal)
 
             case "Welcome Page Setup":
-                db_cog = self.bot.get_cog("Database")
-                select_menu = ChannelSelect(self.bot, db_cog, interaction.guild_id, "welcome")
-                await select_menu.set_options_from_db()
-                if not select_menu.options:
+                # Using Database cog's handle_channel method
+                channel_names = await db_cog.handle_channel(interaction.guild.id, "get_display_names")
+                if not channel_names:
                     await interaction.response.send_message("No channels are available for mapping. Please set up channels first.", ephemeral=True)
                     return
+                select_menu = ChannelSelect(self.bot, channel_names, interaction.guild.id, "welcome")
                 view = discord.ui.View()
                 view.add_item(select_menu)
                 await interaction.response.send_message("Please select a welcome channel:", view=view, ephemeral=True)
 
             case "Support Ticket Setup":
-                db_cog = self.bot.get_cog("Database")
-                select_menu = ChannelSelect(self.bot, db_cog, interaction.guild_id, "support")
-                await select_menu.set_options_from_db()
-                if not select_menu.options:
+                # Using Database cog's handle_channel method
+                channel_names = await db_cog.handle_channel(interaction.guild.id, "get_display_names")
+                if not channel_names:
                     await interaction.response.send_message("No channels are available for mapping. Please set up channels first.", ephemeral=True)
                     return
+                select_menu = ChannelSelect(self.bot, channel_names, interaction.guild.id, "support")
                 view = discord.ui.View()
                 view.add_item(select_menu)
                 await interaction.response.send_message("Please select a support channel:", view=view, ephemeral=True)
@@ -102,14 +102,18 @@ class FAQRemoveSelect(Select):
         self.bot = bot
 
     async def callback(self, interaction):
+        # Convert the selected FAQ number to integer
         faq_number = int(self.values[0])
         try:
+            # Get the Database cog and remove the FAQ from the database
             database_cog = self.bot.get_cog("Database")
-            await database_cog.remove_faq(faq_number, interaction.guild.id)
+            await database_cog.handle_faq(interaction.guild.id, "remove", number=faq_number)
 
+            # Send a success message
             await interaction.response.send_message(f"FAQ #{faq_number} has been removed successfully.", ephemeral=True)
             print(f"FAQ #{faq_number} has been removed successfully.")
         except Exception as e:
+            # Send an error message in case of exceptions
             await interaction.response.send_message(f"Error removing FAQ: {e}", ephemeral=True)
             print(f"Error removing FAQ: {e}")
 
@@ -145,37 +149,43 @@ class AddFAQModal(Modal):
 
     async def on_submit(self, interaction):
         try:
+            # Get the entered FAQ information
             number = int(self.number_input.value)
             name = self.name_input.value
             content = self.content_input.value
+            # Get the Database and Embed cogs
+            database_cog = self.bot.get_cog("Database")
             embed_cog = self.bot.get_cog("CreateEmbed")
 
-            database_cog = self.bot.get_cog("Database")
-            existing_faq = await database_cog.get_faq(number, interaction.guild.id)
-            
+            # Check if the FAQ already exists
+            existing_faq = await database_cog.handle_faq(interaction.guild.id, "get", number=number)
+
             if existing_faq:
+                # Send an error message if the FAQ exists
                 embed = await embed_cog.create_embed(title="Error", description=f"FAQ #{number} already exists. Choose another number.", color=Colour.red())
                 await interaction.response.send_message(embed=embed, ephemeral=True)
                 return
 
-            await database_cog.add_faq(number, name, content, interaction.guild.id)
+            # Add the new FAQ to the database
+            await database_cog.handle_faq(interaction.guild.id, "add", number=number, name=name, content=content)
 
+            # Send a success message
             embed = await embed_cog.create_embed(title="Success", description=f"FAQ #{number} - {name} has been added successfully.", color=Colour.green())
             await interaction.response.send_message(embed=embed, ephemeral=True)
         except ValueError:
+            # Handle ValueError for incorrect FAQ number
             embed = await embed_cog.create_embed(title="Error", description=f"Please enter a valid number for the FAQ.", color=Colour.red())
             await interaction.response.send_message(embed=embed, ephemeral=True)
         except Exception as e:
+            # Handle general exceptions
             embed = await embed_cog.create_embed(title="Error", description=f"Error adding FAQ: {e}", color=Colour.red())
             await interaction.response.send_message(embed=embed, ephemeral=True)
-
 
 
 class RoleMappingModal(Modal):
     def __init__(self, bot, guild):
         super().__init__(title="Map Roles and Create Buttons")
         self.bot = bot
-        
         role_data = self.get_roles_formatted(guild)
         
         # Creating input fields for role and button mapping
@@ -204,6 +214,7 @@ class RoleMappingModal(Modal):
 
     @staticmethod
     def get_roles_formatted(guild):
+        # Format the roles for display
         roles_formatted = [f"{role.name}:{role.id}" for role in guild.roles]
         return ', '.join(roles_formatted)
 
@@ -227,7 +238,7 @@ class RoleMappingModal(Modal):
         embed_cog = self.bot.get_cog("CreateEmbed")
         try:
             database_cog = self.bot.get_cog("Database")
-            await database_cog.set_server_role(interaction.guild.id, button_name, role_name, role_id, emoji)
+            await database_cog.handle_server_role(interaction.guild.id, "set", button_name, role_name, role_id, emoji)
             
             embed = await embed_cog.create_embed(title="Success", description=f"Role and button mapping for '{button_name}' added successfully!", color=Colour.green())
             await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -242,14 +253,15 @@ class ActualRoleSelect(Select):
         self.bot = bot
         self.predefined_name = predefined_name
 
-    async def callback(self, interaction):
+    async def callback(self, interaction: discord.Interaction):
         selected_server_role_id = int(self.values[0])
         role = discord.utils.get(interaction.guild.roles, id=selected_server_role_id)
         
         if role:
             try:
                 database_cog = self.bot.get_cog("Database")
-                await database_cog.set_server_role(interaction.guild.id, self.predefined_name, role.name, role.id)
+                # Update the server role in the database using the new Database cog's method
+                await database_cog.handle_server_role(interaction.guild.id, "set", self.predefined_name, role.name, role.id, None)
 
                 await interaction.response.send_message(f"Role linked to {role.name}.", ephemeral=True)
             except Exception as e:
@@ -298,7 +310,7 @@ class ChannelConfigModal(Modal):
         channel_name, channel_id = channel_data[0], channel_data[1]
         try:
             database_cog = self.bot.get_cog("Database")
-            await database_cog.set_server_channel(interaction, interaction.guild.id, display_name, channel_name, int(channel_id))
+            await database_cog.handle_channel(interaction.guild.id, "set_mapping", display_name, channel_name, int(channel_id), None, None)
             embed = await embed_cog.create_embed(title="Success", description=f"Channel '{display_name}' set successfully.", color=Colour.green())
             await interaction.response.send_message(embed=embed, ephemeral=True)
         except Exception as e:
@@ -338,7 +350,9 @@ class ChannelSelect(Select):
         self.options = []
 
     async def set_options_from_db(self):
-        channel_info = await self.db_cog.get_channel_info(self.guild_id)
+        # Get channel info from the new Database cog's method
+        channel_info = await self.db_cog.handle_channel(self.guild_id, "get_channel_info")
+        # Populate the dropdown options
         self.options = [discord.SelectOption(label=name, value=json.dumps({"name": name, "id": channel_id})) for name, channel_id in channel_info]
 
     async def callback(self, interaction):
